9c536875436e2999dbb1c0ccffc241d5fcc16f6a
kryptonite <moonbit-1@protonmail.com>
2023-03-14 12:16:55 -0600

    first commit

---
+++ books.php
@@ -0,0 +1,21 @@
+<?php include('template/header.php');?>
+
+<div class="col-4">
+    <div class="card">
+      <div class="card-body">
+        <h3 class="card-title">Title</h3>
+        <p class="card-text">Text</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-4">
+    <div class="card">
+      <div class="card-body">
+        <h3 class="card-title">Title</h3>
+        <p class="card-text">Text</p>
+      </div>
+    </div>
+</div>
+
+<?php include('template/footer.php');?>
---
+++ template/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Website</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="books.php">Books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<br/>
+<div class="container">
+    <div class="row">
